58280a09476abadfb26a31435732956f81fb1f7a
Jhonata <jholzinhooh567@gmail.com>
2024-06-05 12:45:29 -0300

    "closes #3,closes #4 - Modal e Usabilidade"

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,42 @@
-
-
 function calc() {
     var quantities = document.getElementsByName("quantity");
     var foodName = document.getElementsByClassName("food-description")
     var foodPrice = document.getElementsByClassName("price-valor")
 
     var output = document.getElementById("output");
-    var name = document.getElementById("name").value;
-    var total = 0;
+    var name = document.getElementById("inputNome").value
+    var total = 0
 
-    output.innerHTML = "";
-    output.innerHTML = `<div style="margin: 2rem 0 2rem 0;">Caro <span class="name-calc">${name}.</span> </div> 
-                        Seguem os dados do seu pedido.<p>
-                        o seu pedido é:<p>`
 
     var formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
     });
+    var text = output.innerHTML = `Caro <span class="name-calc"><strong>${name}.</strong></span> <br>
+    Seguem os dados do seu pedido.<p>
+    o seu pedido é:<p>`
 
     for (var input of quantities) {
-        if (input.value > 0) {
 
+
+        if (input.value >= 1) {
             var id = input.id
             output.innerHTML += `<li>Prato: ${foodName[id - 1].textContent}  - Preço unitário: R$${foodPrice[id - 1].textContent} - Quantidade: ${input.value} - Total: R$${parseFloat(foodPrice[id - 1].textContent) * parseFloat(input.value)} </li>`;
             total += parseFloat(foodPrice[id - 1].textContent) * parseFloat(input.value)
+
+
         }
 
     }
     output.innerHTML += `<h3><br>Preço final: ${formatter.format(total)}</h3> `;
+    if (total <= 0.00) {
+        output.innerHTML = `<p class="text-danger fs-3 text-center"><strong>Selecione pelo menos um prato!</strong></p>`
+
+    }
+
+
+
+
 
 
 
